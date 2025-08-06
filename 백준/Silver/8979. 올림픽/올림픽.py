f397f24e8a29a46c352f45da8a6a82a184cbb320
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-08-06 11:10:43 +0900

    [Silver V] Title: 올림픽, Time: 112 ms, Memory: 110576 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -11,20 +11,23 @@ result_list = [0] * 1001
 for i in range(N):
     num, gold, silver, dong = map(int, input().split())
     num_list.append((gold, silver, dong, num))
-# print(num_list)
 
 num_list.sort(key= lambda x : (-x[0], -x[1], -x[2]))
 
-# print(num_list)
 count = 0
+i = 0
+
+e,f,g = -1,-1,-1
+next_count = 1
 
 for num in num_list:
     a,b,c,d = num
-    if ((a,b,c) in temp_list):
+    if ((a,b,c) == (e,f,g)):
         result_list[d] = count
-        count += 1
+        next_count += 1
     else:
-        temp_list.append((a,b,c))
-        count += 1
+        e,f,g = a,b,c
+        count = count + next_count
+        next_count = 1
         result_list[d] = count
 print(result_list[K])
