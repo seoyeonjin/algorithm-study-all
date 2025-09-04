8d3317774aa282ad2b53aea172113e59bb3bfe7f
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-09-05 01:04:45 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -10,50 +10,50 @@ def solution(s):
     i = 0
     while i < len(s):
         a = s[i]
-        if (is_num(a)):
+        if (a.isdigit()):
             answer += a
             i += 1
         else:
-            if (a == "z"):
-                answer += "0"
-                i += 4
-            elif (a == "o"):
-                answer += "1"
-                i += 3
-            elif (a == "t"):
-                if (s[i+1] == "w"):
-                    answer += "2"
-                    i += 3
-                else:
-                    answer += "3"
-                    i += 5
-            elif (a == "f"):
-                if (s[i+1] == "o"):
-                    answer += "4"
-                    i += 4
-                else:
-                    answer += "5"
-                    i += 4
-            elif (a == "s"):
-                if (s[i+1] == "i"):
-                    answer += "6"
-                    i += 3
-                else:
-                    answer += "7"
-                    i += 5
-            elif (a == "e"):
-                answer += "8"
-                i += 5
-            else:
-                answer += "9"
-                i += 4
-            # temp = ""
-            # b = i
-            # while (b < len(s) and not is_num(s[b])):
-            #     temp += s[b]
-            #     b += 1
-            #     if (temp in dic):
-            #         answer += str(dic[temp])
-            #         i = b
-            #         break
+            # if (a == "z"):
+            #     answer += "0"
+            #     i += 4
+            # elif (a == "o"):
+            #     answer += "1"
+            #     i += 3
+            # elif (a == "t"):
+            #     if (s[i+1] == "w"):
+            #         answer += "2"
+            #         i += 3
+            #     else:
+            #         answer += "3"
+            #         i += 5
+            # elif (a == "f"):
+            #     if (s[i+1] == "o"):
+            #         answer += "4"
+            #         i += 4
+            #     else:
+            #         answer += "5"
+            #         i += 4
+            # elif (a == "s"):
+            #     if (s[i+1] == "i"):
+            #         answer += "6"
+            #         i += 3
+            #     else:
+            #         answer += "7"
+            #         i += 5
+            # elif (a == "e"):
+            #     answer += "8"
+            #     i += 5
+            # else:
+            #     answer += "9"
+            #     i += 4
+            temp = ""
+            b = i
+            while (b < len(s) and not is_num(s[b])):
+                temp += s[b]
+                b += 1
+                if (temp in dic):
+                    answer += str(dic[temp])
+                    i = b
+                    break
     return int(answer)
