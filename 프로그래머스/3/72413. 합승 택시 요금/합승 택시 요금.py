25f765f94be557370326b0d6c975acc248406d5d
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-10-06 16:14:53 +0900

    [level 3] Title: 합승 택시 요금, Time: 2925.06 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -0,0 +1,33 @@
+import heapq
+
+def solution(n, s, a, b, fares):
+    
+    INF = float('inf')
+    answer = 0
+    dist = [[INF for i in range(n+1)] for j in range(n+1)]
+
+    for i in range(1, n+1):
+        dist[i][i] = 0
+
+    for (c, d, w) in fares:
+        dist[c][d] = w
+        dist[d][c] = w
+
+   
+    for k in range(1,n+1):        
+        for i in range(1,n+1):   
+            for j in range(1,n+1):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    
+    # for d in dist:
+    #     print(d)
+    min_cnt = 10 **10
+    for i in range(1,n+1):
+        cnt = dist[s][i] + dist[i][a] + dist[i][b]
+        min_cnt = min(cnt, min_cnt)
+        # print(i, cnt)
+    answer = min_cnt
+        
+        
+    
+    return answer
