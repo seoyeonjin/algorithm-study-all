50ccebef80623929226f2d5200005d9eaef38cc3
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-10-09 01:23:20 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 59.22 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,36 +1,57 @@
+from collections import deque
+
 def solution(queue1, queue2):
-    answer = -2 
-    all_que = queue1 + queue2
-    # print(all_que)
-    start = 0
-    end = len(queue1) 
+    answer = -2
+    q1 = deque(queue1)
+    q2 = deque(queue2)
     
-    qsum = sum(queue1)
-    target = qsum + sum(queue2)
-    if (target % 2 == 1): 
-        return -1
-    target = int(target//2)
+    que = deque()
     cnt = 0
-    flag = False
-    # print(target)
-    while start <= end and end < len(all_que):
-        if (qsum < target):
-            qsum += all_que[end]
-            end += 1
-            cnt += 1
-        elif (qsum > target): 
-            qsum -= all_que[start]
-            start += 1
-            cnt += 1
-        else:
-            flag = True
-            break
-    
-    if (flag):
-        answer = cnt
-    else: 
-        answer = -1
+    q1_sum = sum(q1)
+    q2_sum = sum(q2)
+    que.append((q1,q2,cnt, q1_sum, q2_sum))
+      
+    while que:
+        # print(que)
+        q1, q2, cnt,s1,s2 = que.popleft()
+        if (q1 == queue1 and cnt != 0):
+            return -1   
+        if (len(q1) == 0 or len(q2) == 0):
+            return -1
         
-            
-    # 실제로 하면 안 되나? 
+        if (s1 < s2):
+            num = q2.popleft()
+            q1.append(num)
+            s1 = s1 + num
+            s2 = s2 - num
+            que.append((q1,q2, cnt+1, s1,s2))
+        elif (s2 < s1):
+            num = q1.popleft()
+            q2.append(num)
+            s1 -= num
+            s2 += num
+            que.append((q1,q2, cnt+1, s1, s2))
+        else:
+            return cnt
+        
+        
+#     total_que = queue1 + queue2
+#     # sum_total = sum(total_que)
+#     half = len(queue1)
+    
+#     prefix_sum = [0] * (half * 2 + 1)
+#     for i in range(half*2):
+#         prefix_sum[i+1] = prefix_sum[i] + total_que[i]
+#     print(prefix_sum)
+    
+#     total_sum = prefix_sum[-1]
+    
+#     for i in range(half+1):
+#         half_sum = prefix_sum[i+half] - prefix_sum[i]
+#         print(half_sum)
+#         if (half_sum == total_sum // 2):
+#             print(i+1)
+#             return (i+1)
+#     return -1
+        
     return answer
