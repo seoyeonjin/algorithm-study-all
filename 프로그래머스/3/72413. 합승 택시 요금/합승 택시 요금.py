24cfd516b54bcbe3c261b7154bbad162b18152f4
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-10-23 18:38:55 +0900

    [level 3] Title: 합승 택시 요금, Time: 2530.86 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,31 +1,32 @@
-import heapq
-
 def solution(n, s, a, b, fares):
     
     INF = float('inf')
-    answer = 0
-    dist = [[INF for i in range(n+1)] for j in range(n+1)]
-
-    for i in range(1, n+1):
-        dist[i][i] = 0
-
-    for (c, d, w) in fares:
-        dist[c][d] = w
-        dist[d][c] = w
-
-   
-    for k in range(1,n+1):        
-        for i in range(1,n+1):   
-            for j in range(1,n+1):
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-
-    min_cnt = float('inf')
-    for i in range(1,n+1):
-        cnt = dist[s][i] + dist[i][a] + dist[i][b]
-        min_cnt = min(cnt, min_cnt)
-        # print(i, cnt)
-    answer = min_cnt
-        
-        
+    answer = INF
+    # 플로이드 워샬
+    root = [[INF for i in range(n+1)] for j in range(n+1)]
     
+    for f in fares:
+        e,d,c = f
+        root[e][d] = c
+        root[d][e] = c
+        
+    for i in range(n+1):
+        for j in range(n+1):
+            if (i == j):
+                root[i][j] = 0
+    
+    
+    for k in range(n+1):
+        for i in range(n+1):
+            for j in range(n+1):
+                root[i][j] = min(root[i][j], root[i][k] + root[k][j])
+                
+    # a는 a도착
+    # b는 b도착
+    # s는 출발
+    # 어디까지 합승하는 게 좋을까? 
+    for i in range(1, n+1): # 하나씩 합승 지점이다
+        dis = root[s][i] + root[i][a] + root[i][b]
+        answer = min(answer, dis)
+    # print(root)
     return answer
