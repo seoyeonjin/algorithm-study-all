e2874412f9e5cdf0f00664e6a830f7408fcf34f5
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-10-24 11:00:43 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 152 ms, Memory: 114828 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+T = int(input())
+
+for t in range(T):
+    N = int(input())
+    con_list = []
+    sx, sy = map(int, input().split())
+
+    for _ in range(N):
+        x, y = map(int, input().split())
+        con_list.append((x,y))
+
+    ends = list(map(int, input().split()))
+    ex, ey = ends
+    con_list.append((ex,ey))
+
+    que  = deque()
+    que.append((sx,sy))
+    visited = [(sx,sy)]
+
+    result = False
+    while que:
+        x, y = que.popleft()
+        if (x == ex and y == ey):
+        # if (abs(x-ex) + abs(y-ey) <= 1000):
+            result = True
+            break
+        for i in range(N+1): # 편의점
+            cx,cy = con_list[i]
+            if (abs(x-cx) + abs(y-cy) <= 1000 and (cx,cy) not in visited): # 갈 수 있으면
+                visited.append((cx,cy))
+                que.append((cx,cy))
+    
+    if (result):
+        print("happy")
+    else:
+        print("sad")
+
+
