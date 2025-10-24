0811f8e850ecff3b156ff920e90f82ce48016137
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-10-24 14:35:35 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 875.48 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,45 +1,52 @@
 def solution(edges):
     answer = []
-    node = [[0,0] for i in range(1000001)] # 초기화
-    # 각 노드별 들어오는 거, 나가는 거
     
-    start = -1
-    for e in edges:
-        a,b= e
-        node[a][1] += 1
-        node[b][0] += 1
-    # 나가는 점이 2개 이상 있는 데 들어오는 점은 없는 점 -> 생성한 정점
-    # 나가는 화살표의 개수가 전체 모양의 개수다 ! 
-    for i in range(len(node)):
-        n = node[i]
-        if (n[0] == 0 and n[1] >= 2): 
-            start = i
-    total_cnt = node[start][1]
-    # print("total_cnt: ", total_cnt)
+    # edges를 인접 리스트로 표현
+    # max_n = max(sum(edges, []))
     
-    # print(node)
-    bar_cnt = 0
+    max_n = -1
     for e in edges:
         a,b = e
-        if (a == start):
-            node[b][0] -= 1
-            if (node[b][0] == 0 and node[b][1] == 0):
-                bar_cnt += 1
+        max_n = max(max_n, a,b)
+
+    # print(max_n)
+    redges = [[0,0] for i in range(max_n+1)] # 들어오는 거, 나가는 거
     
-    # 8자 모양 그래프의 개수는? 
-    eight_cnt = 0
-    for n in node:
-        if (n[0] == 2 and n[1] == 2): 
-            eight_cnt += 1
+    # print(redges)
+    for e in edges: # 들어오는 거 나가는 거 표시
+        a,b = e
+        redges[a][1] += 1
+        redges[b][0] += 1
+        
     
-    # 막대 모양 그래프의 개수는? 
+    for i in range(1, len(redges)): # root 찾기
+        r = redges[i] 
+        if (r[0] == 0 and r[1] >= 2):
+            root = i
+            total = r[1]
+        if (r[0] == 0 and r[1] == 0):
+            redges[i] = None
+    # print(root, total)
     
-    for n in node:
-        if (n[0] == 0 and n[1] == 1): 
-            bar_cnt += 1
+    for e in edges: #
+        if (e[0] == root):
+            redges[e[1]][0] -= 1
+        
+    # print(redges)
     
-    donut_cnt = total_cnt - eight_cnt - bar_cnt
-    answer = [start, donut_cnt, bar_cnt, eight_cnt]
-    # print(node)
-   
+    eight = 0
+    donut = 0
+    bar = 0
+    
+    for r in redges[1:]:
+        if (r == None):
+            continue
+        if (r[0] == 2 and r[1] == 2): 
+            eight += 1
+        if (r[1] == 0):
+            bar += 1
+    
+    # bar -= 1 # root는 빼줌
+    
+    answer = [root, total - bar -eight, bar, eight]
     return answer
