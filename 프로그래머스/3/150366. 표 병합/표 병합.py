03212a4ce238d8fedfb59c1752a486bd9002b871
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-10-25 19:17:11 +0900

    [level 3] Title: 표 병합, Time: 1043.90 ms, Memory: 9.69 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.py
+++ 프로그래머스/3/150366. 표 병합/표 병합.py
@@ -1,128 +1,96 @@
-# import sys 
-# sys.setrecursionlimit(1000)
-
+# import sys
+# sys.setrecursionlimit(100000)
 def solution(commands):
     answer = []
-    root = [[[i,j] for j in range(51)] for i in range(51)]
-    values = [["" for i in range(51)] for j in range(51)]
+    values = [["" for i in range(51)] for j in range(51)] # 값을 가지는 것
+    roots = [[[i, j] for j in range(51)] for i in range(51)]
     
-    def ufind(r,c, value):
-        values[r][c] = value
-        if (root[r][c][0] == r and root[r][c][1] == c):
-            return [r,c]
-        else:
-            result = ufind(root[r][c][0], root[r][c][1], value)
-            root[r][c][0] = result[0]
-            root[r][c][1] = result[1]
-            return result
-        
-    def ufind_wo_value(r,c):
-        if (root[r][c][0] == r and root[r][c][1] == c):
-            return [r,c]
-        else:
-            result = ufind_wo_value(root[r][c][0], root[r][c][1])
-            root[r][c][0] = result[0]
-            root[r][c][1] = result[1]
-            return result
     
-    def merge(a,b,c,d):
-        # union을 구현한다. 
-        # if (a == c and b ==d):
-        #     return
+    def ufind(r,c): # union 임 (root만 값 갱신함)
+        if (roots[r][c] != [r,c]):
+            roots[r][c] = ufind(roots[r][c][0], roots[r][c][1])
+        return roots[r][c]
         
-        if (values[a][b]):
-            value = values[a][b]
-        else:
-            value = values[c][d]
-        
-        root1 = ufind(a,b, value)
-        root2 = ufind(c,d, value)
-        
-        # print(root1, root2)
-        
-        minr = min(root1, root2)
-        maxr = max(root1, root2)
-        # print(minr, maxr)
-        
-        for i in range(51):
-            for j in range(51):
-                if (root[i][j] == maxr or root[i][j] == minr):
-                    root[i][j] = minr
-                    values[i][j] = value
-        
-        # if (root1[0] < root2[0]): # 더 작은 거에 담아둔다
-        #      root[root2[0]][root2[1]] = root1
-        # elif (root1[0] == root2[0]):
-        #     if (root1[1] <= root2[1]): # 둘이 같을 때는?
-        #         root[root2[0]][root2[1]] = root1
-        #     else: 
-        #         root[root1[0]][root1[1]] = root2
-        # else:
-        #     root[root1[0]][root1[1]] = root2
-            
-            
-        # print(root[1][2], root[1][3])
-        
-    # def ufind(r,c,value):
-    def update_all(v1, v2):  # v1을 찾는다
+    def update_all(v1, v2): # 값 갱신
         for i in range(51):
             for j in range(51):
                 if (values[i][j] == v1):
                     values[i][j] = v2
-        
     
-    def update(r,c,value):
-        values[r][c] = value
-        r = ufind(r,c,value)
-        # print(r[0], r[1])
-        for i in range(51):
-            for j in range(51):
-                if (root[i][j] == [r[0], r[1]]):
-                    values[i][j] = value
+    def merge(r1,c1,r2,c2): # merge => 유사 union
+        value = values[r1][c1] # 병합되고 저장될 값
+        if (value == ""): # 가지고 있는 값이 없으면?
+            value = values[r2][c2]
+            
+        # 각자의 root를 찾는다. 
+        root1 = ufind(r1, c1)
+        root2 = ufind(r2, c2)
+        
+        # 어디에 병합할거냐?
+        if root1 < root2: # root1이 더 작으면 -> 큰게 바뀌어야지
+            roots[root2[0]][root2[1]] = root1
+            return root1, value
+        elif (root1 > root2):
+            roots[root1[0]][root1[1]] = root2
+            return root2, value
+        else:
+            return root1, value
+        
+        
     def unmerge(r,c):
-        # 셀의 모든 병합을 해제한다. 
+        # 병합을 해제한다. -> root를 찾는다.
         value = values[r][c]
-        root1 = ufind_wo_value(r,c) # 이 root를 가지고 있는 애 찾아서 다 value ""으르 변경하기 root도 자기자신으로 변경
-        # a, b = root1
+        root = ufind(r,c) # root를 찾음
         
         for i in range(51):
             for j in range(51):
-                if (root[i][j] == root1):
+                if (ufind(roots[i][j][0], roots[i][j][1]) == root):
                     values[i][j] = ""
-                    root[i][j] = [i,j]
+                    roots[i][j] = [i,j]
         values[r][c] = value
         
-
-    for c in commands:
-        cmds = c.split(" ")
-        if (cmds[0] == "UPDATE" and len(cmds) == 4): #  셀 업데이트
-            r= int(cmds[1])
-            c = int(cmds[2])
-            update(r,c,cmds[3])
+    for cmd in commands:
+        cmds = list(cmd.split(" "))
+        
+        if (cmds[0] == "UPDATE" and len(cmds) == 4):
+            # r, c 에 value로 업데이트
+            r, c = int(cmds[1]), int(cmds[2])
+            root = ufind(r,c)
+            for i in range(51):
+                for j in range(51):
+                    if (ufind(roots[i][j][0], roots[i][j][1]) == root):
+                        values[i][j] = cmds[3]
         elif (cmds[0] == "UPDATE" and len(cmds) == 3):
-            v1 = cmds[1]
-            v2 = cmds[2]
-            update_all(v1, v2)
-            # print()
+            update_all(cmds[1], cmds[2])
         elif (cmds[0] == "MERGE"):
-            r1 = int(cmds[1])
-            c1 = int(cmds[2])
-            r2 = int(cmds[3])
-            c2 = int(cmds[4])
-            merge(r1,c1,r2,c2)
+            r1, c1, r2, c2 = int(cmds[1]), int(cmds[2]), int(cmds[3]), int(cmds[4])
+            root, value = merge(r1,c1,r2,c2)
+            for i in range(51):
+                for j in range(51):
+                    if (ufind(roots[i][j][0], roots[i][j][1]) == root) :
+                        values[i][j] = value
+                        roots[i][j] = root
         elif (cmds[0] == "UNMERGE"):
-            r = int(cmds[1])
-            c = int(cmds[2])
+            r, c = int(cmds[1]), int(cmds[2])
             unmerge(r,c)
-        else: # print
-            r = int(cmds[1])
-            c = int(cmds[2])
-            if (values[r][c] == ""):
+        elif (cmds[0] == "PRINT"):
+            r, c = int(cmds[1]), int(cmds[2])
+            value = values[r][c]
+            if (value == ""):
                 answer.append("EMPTY")
             else:
-                answer.append(values[r][c])
-    
-        # for v in values[:6]:
-        #     print(v[:6])
+                answer.append(value)
+          
+#         print(cmd)
+#         for v in roots[:5]:
+#             print(v[:5])
+        
             
+    # UPDATE r c value
+    # UPDATE v1 v2 -> 모든 v1을 v2로 변경 -> root에 가서 변경하기만 하면 됨
+    # MERGE r1 c1 r2 c2 -> 합친다. -> 둘 중 하나만 값이 있을 경우 그 값을 가짐, 아닌 경우 r1, c1위치의 값을 가지게 됨 r2 c2의 root를 찾아서 r1 c1을 보게 하면 됨
+    # UNMERGE r c -> 모든 병합을 해제 -> 값은 r,c 위치에
+    # PRINT r c -> 값 출력
+    
+    
     return answer
