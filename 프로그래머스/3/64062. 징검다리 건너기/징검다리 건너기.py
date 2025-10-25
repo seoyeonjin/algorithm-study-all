55fe1868c479307da3328cf40f2c65af0fbc6b01
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2025-10-25 22:41:05 +0900

    [level 3] Title: 징검다리 건너기, Time: 317.43 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,19 +1,40 @@
-from collections import deque
+import heapq
 
 def solution(stones, k):
-    max_arr = []
-    q = deque()
-    q.append((stones[0], 0))
-    max_arr.append(stones[0])
-
-    for i in range(1, len(stones)):
-        while q and q[-1][0] < stones[i]:
-            q.pop()
-        q.append((stones[i], i))
-
-        while i - q[0][1] >= k:
-            q.popleft()
-
-        max_arr.append(q[0][0])
-
-    return min(max_arr[k-1:])
+    answer = 0
+    max_list = []
+    
+    # 구간 k 중에 가장 큰 값을 어떻게 효과적으로 찾을 수 있지..?
+    # 우선순위 큐로 저장하면 자동으로 가장 큰 값이 나오지 않나?
+    # 
+    start = 0 
+    end = k
+    answer = max(stones[start:end]) # 초기화
+    heap = []
+    
+    
+    for i in range(start, end):
+        heapq.heappush(heap, (-stones[i], i))
+        
+    
+    while end < len(stones): # 이동하면서
+        # heap.remove(-stones[start])
+        # print(answer)
+        start += 1
+        heapq.heappush(heap, (-stones[end], end))
+        
+        while heap:
+            h = heap[0][1]
+            max_num = -heap[0][0]
+            # print(heap[0],start)
+            if (h < start):
+                heapq.heappop(heap)
+            else:
+                break
+          
+        # print(start, heap)
+        answer = min(-heap[0][0], answer)
+        end += 1
+    
+    
+    return answer
