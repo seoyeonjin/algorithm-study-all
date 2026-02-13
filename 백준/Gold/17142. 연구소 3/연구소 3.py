e1d8b54977e67232e1eca5ec2585f993930bdb30
서연진(promedev) <68274803+seoyeonjin@users.noreply.github.com>
2026-02-13 23:08:51 +0900

    [Gold III] Title: 연구소 3, Time: 280 ms, Memory: 119268 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,80 +1,75 @@
-# 복제에 1초 걸린다.
-# 바이러스 M개를 활성 상태로 변경한다
+# N,M
 
-# 0은 빈칸, 1은 벽, 2는 바이러스다
-# 활성 바이러스 -> 비활성 바이러스 칸
+# 모든 바이러스는 비활성 상태
 
-# 활성칸 정보 찾기
-
+# 활성 상태로 변경한다.
 from itertools import combinations
 from collections import deque
+from copy import deepcopy
 
-n, m = map(int, input().split())
+
+N,M = map(int,input().split())
 nums = []
-vis = []
+vs = []
 
-for i in range(n):
-    nums.append(list(map(int, input().split())))
 
-for i in range(n):
-    for j in range(n):
+for i in range(N):
+    nums.append(list(map(int,input().split())))
+
+for i in range(N):
+    for j in range(N):
         if (nums[i][j] == 2):
-            vis.append((i,j))
+            vs.append((i,j))
 
-
-coms = combinations(vis, m)
-min_cnt = 1e9
+coms = combinations(vs, M) # M개 뽑아서 활성 상태로 변경하기
 
 dx = [0,0,-1,1]
 dy = [1,-1,0,0]
 
-def bfs(coms):
-   
-    # cnt = 0
+# zero 개수 세기
+zcnt = sum(nums, []).count(0)
+# print(zcnt)
+ans = []
+
+
+# 활성 상태는 3으로 표시한다.
+for com in coms:
+    # 활성 상태로 놓고 시작하기
     que = deque()
     visited = set()
-    zero = 0
-    ns = nums # deepcopy 개선의 레거시
+    zero = zcnt
+    dnums = deepcopy(nums)
 
-    for i in range(n):
-        for j in range(n):
-            if (ns[i][j] == 0):
-                zero += 1
-    for c in coms:
-        que.append((c[0],c[1], 0))
-        visited.add(c)
+    if (zero == 0):
+        ans.append(zero)
+        break
 
-    # visited.add(coms)
-    # print(visited)
-
-    while que:  
-        # print(que)
+    for c in com:
+        que.append((c[0], c[1], 0))
+        dnums[c[0]][c[1]] = 3
+    while que:
         x, y, cnt = que.popleft()
-        if (zero == 0):
-            return cnt
         for d in range(4):
             nx = dx[d] + x
             ny = dy[d] + y
-            if (0<=nx<n and 0<=ny<n and (nx,ny) not in visited):
-                if (ns[nx][ny] == 0):
+            if (0<=nx<N and 0<=ny<N and (nx,ny) not in visited):
+                if (dnums[nx][ny] == 0):
+                    dnums[nx][ny] = 3
+                    visited.add((nx,ny))
+                    que.append((nx,ny,cnt+1))
                     zero -= 1
                     if (zero == 0):
-                        return cnt +1
-                    que.append((nx,ny,cnt+1))
+                        ans.append(cnt+1)
+                        break
+                elif (dnums[nx][ny] == 2):
+                    dnums[nx][ny] = 3
                     visited.add((nx,ny))
-                elif(ns[nx][ny] == 2 or ns[nx][ny] == 3):
                     que.append((nx,ny,cnt+1))
-                    visited.add((nx,ny))
-                    # ns[nx][ny] = 3
-    return 1e9
-            
 
-for com in coms:
-    cnt = bfs(com)
-    # print(cnt)
-    min_cnt = min(min_cnt, cnt)
-
-if (min_cnt == 1e9):
+if (len(ans) == 0):
     print(-1)
 else:
-    print(min_cnt)
+    ans.sort()
+    print(ans[0])
+                
+    
