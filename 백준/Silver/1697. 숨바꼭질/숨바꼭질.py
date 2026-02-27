ae5fc994b859109c9048e7f5a5964672b105e67e
서연진(promedev) <68274803+seoyeonjin@users.noreply.github.com>
2026-02-27 09:49:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 114664 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,33 @@
+# 수빈, 동생
+# 수빈은 X -> X-1, X+1, 2*X 로 이동
+
+# 수빈과 동생으 ㅣ위치 -> 가장 빠른 시간 몇 초?
+from collections import deque
+
+N,M= map(int, input().split())
+
+ml = 150000
+
+dp = [1e9] * (ml)
+
+que = deque()
+que.append(N)
+dp[N] = 0
+
+while que:
+    node = que.popleft()
+    if (node == M):
+        print(dp[node])
+        break
+    if (node + 1 < ml and dp[node+1] == 1e9):
+        dp[node+1] = min(dp[node+1], dp[node] + 1)
+        que.append(node+1)
+    if (node - 1 >= 0 and dp[node-1] == 1e9):
+        dp[node-1] = min(dp[node-1], dp[node] + 1)
+        que.append(node-1)
+    if (node * 2 < ml and dp[node*2] == 1e9):
+        dp[node*2] = min(dp[node*2], dp[node] + 1)
+        que.append(node*2)
+
+# print(dp[M])
+    
