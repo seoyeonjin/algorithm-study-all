98916224db89fcba6c7fc3ad930947a2715d7f5d
서연진(promedev) <68274803+seoyeonjin@users.noreply.github.com>
2026-03-13 23:34:18 +0900

    [Silver I] Title: IOIOI, Time: 96 ms, Memory: 110712 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -31,26 +31,49 @@ for i in range(len(S)):
     else:
         bits += "1"
     
-# print(bits)
+# # print(bits)
 
-com = ""
+# com = ""
+
+# cnt = 0
+
+# for i in range(2*N+1):
+#     if (i % 2):
+#         com += "0"
+#     else:
+#         com += "1"
+# com = int(com)
+
+
+# for i in range(len(S)-(2*N)):
+#     temp = bits[i:i+(2*N+1)]
+#     temp = int(temp)
+#     if (temp == com):
+#         cnt += 1
+# print(cnt)
 
 cnt = 0
-
-for i in range(2*N+1):
-    if (i % 2):
-        com += "0"
-    else:
-        com += "1"
-com = int(com)
-
-
-for i in range(len(S)-(2*N)):
-    temp = bits[i:i+(2*N+1)]
-    temp = int(temp)
-    if (temp + com == com * 2):
+temp = 0
+result = 0
+for i in range(len(S)):
+    # 만약에 1 이면 + 1 0이면 -1 하면서 가다가 cnt가 n이랑 같아지면 + 1 하고 계속 간다. 다시 0이 되면 + 1하면서 간다
+    # 만약에 음수가 되거나 2가 되면, 그 자리부터 다시 간다.
+    # print(bits[:i+1])
+    if (bits[i] == "1"):
+        temp += 1
         cnt += 1
-print(cnt)
+    else:
+        temp -= 1
+        cnt += 1
 
+    # print(temp, cnt, result)
+    if (temp == 1 and cnt >= (2*N+1)):
+        result += 1
+    elif (temp < 0): # 이렇게 되면
+        cnt = 0
+        temp = 0
+    elif (temp >= 2):
+        cnt = 1
+        temp = 1
+print(result)
 
-# 더하면 20202가 된다? -> 그러면 더해서 확인해보면 되는 거 아닌가?
