e854d644a434064ef6ef0cfbc855a8cc0dabf3d3
서연진(promedev) <68274803+seoyeonjin@users.noreply.github.com>
2026-03-13 23:59:14 +0900

    [Silver I] Title: IOIOI, Time: 124 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -6,52 +6,6 @@ N = int(input())
 M = int(input()) # s의 길이 
 S = input()
 
-# 문자열 안에 O가 N개 나오고 I가 N+1개 나오는 쌍이 몇 개 있는지 구하기
-# I로 시작하고 I로 끝나야 함
-
-# Pn을 구해서 매칭되는 거 확인하면 되지 않을까?
-# -> 이러면 n^2만큼 돌아야 함
-# # 일단 Pn을 구한다.
-# P = ""
-
-# for i in range(N):
-#     P += "IO"
-# P += "I"
-
-# 비교를 이렇게 하면 안 되는데.. 흐음.. 무슨 방법이 없을까?
-
-# 2N + 1 이 P의 길이임
-# 약간 비트마스킹으로 비교를 해야할 거 같은데?
-# 2N+1 길이의 1이랑 합쳤을 때 -> 기존이랑 같은지 확인하기?
-bits = ""
-
-for i in range(len(S)):
-    if (S[i] == "O"):
-        bits += "0"
-    else:
-        bits += "1"
-    
-# # print(bits)
-
-# com = ""
-
-# cnt = 0
-
-# for i in range(2*N+1):
-#     if (i % 2):
-#         com += "0"
-#     else:
-#         com += "1"
-# com = int(com)
-
-
-# for i in range(len(S)-(2*N)):
-#     temp = bits[i:i+(2*N+1)]
-#     temp = int(temp)
-#     if (temp == com):
-#         cnt += 1
-# print(cnt)
-
 cnt = 0
 temp = 0
 result = 0
@@ -59,7 +13,7 @@ for i in range(len(S)):
     # 만약에 1 이면 + 1 0이면 -1 하면서 가다가 cnt가 n이랑 같아지면 + 1 하고 계속 간다. 다시 0이 되면 + 1하면서 간다
     # 만약에 음수가 되거나 2가 되면, 그 자리부터 다시 간다.
     # print(bits[:i+1])
-    if (bits[i] == "1"):
+    if (S[i] == "I"):
         temp += 1
         cnt += 1
     else:
