69502872bbdfa73a35a6777bb738241f07db243e
서연진(promedev) <68274803+seoyeonjin@users.noreply.github.com>
2026-03-23 23:00:19 +0900

    [Silver III] Title: 피보나치 함수, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+dp = [[0,0] for i in range(41)]
+dp[0][0] = 1
+dp[1][1] = 1
+
+for i in range(2, 41):
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+
+for _ in range(T):
+    a = int(input())
+    print(dp[a][0], dp[a][1])
